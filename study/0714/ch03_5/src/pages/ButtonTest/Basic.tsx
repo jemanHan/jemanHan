f524af552fdfecb8802c42543c9914882ba15e31
Your Name <you@example.com>
2025-07-14 16:47:22 +0900

    0714

--- study/0714/ch03_5/src/pages/ButtonTest/Basic.tsx
+++ study/0714/ch03_5/src/pages/ButtonTest/Basic.tsx
@@ -3,9 +3,9 @@ import {Button} from '../../theme/daisyui'
 export default function Basic() {
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">Basic</h2>
-      <div className="mt-4 flex justify-evenly">
-        <button className="btn btn-primary">DAYSIUI BUTTON</button>
+      <h2 className="text-5xl font-bold text-center">Basic</h2>
+      <div className="flex mt-4 justify-evenly">
+        <button className="btn btn-primary">DAISYUI BUTTON</button>
         <Button className="btn-primary">BUTTON</Button>
       </div>
     </section>
